4b5097815a5970649a870868f7735dca9da24985
Romain Champourlier <pro@rchampourlier.com>
2012-03-29 19:30:20 +0200

    Should not be in ARC branch (master)

--- YmsGradientButton/YmsGradientButton.m
+++ YmsGradientButton/YmsGradientButton.m
@@ -57,8 +57,6 @@
     [NSException raise:@"Invalid YmsGradientButton Configuration" 
                 format:@"Please revise the file %@.plist to confirm that it has legal values.", self.resourceName];
   }
-  
-  [buttonConfig release];
 }
 
 
